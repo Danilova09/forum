4a730038fd447c473fe2fffecba2e8e9a8164959
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-03-10 01:30:09 +0600

    Basic layout and stylization of register form, added validation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
@@ -13,6 +12,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
+import { CenteredCardComponent } from './ui/centered-card/centered-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { ValidateEqualModule } from 'ng-validate-equal';
 
 @NgModule({
   declarations: [
@@ -20,17 +26,24 @@ import { MatButtonModule } from '@angular/material/button';
     NavigationComponent,
     PostsComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    CenteredCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([]),
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    FormsModule,
+    MatInputModule,
+    ValidateEqualModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
